8d19c96e964fa300580eaccea51a3d2911f71cde
Anonymous2910 <68387653+Anonymous2910@users.noreply.github.com>
2023-06-27 00:22:38 +0300

    firstComit

---
+++ yamakasi/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     thIS IS THE FIRST MILESTONE FOR THIS PROJECT
+    </div>
+  );
+}
+
+export default App;
